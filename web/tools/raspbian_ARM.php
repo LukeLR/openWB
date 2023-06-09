9e259ecca4d3506c0142de204aa008d5fd0f7ca2
A git user <public@lrose.de>
2023-06-09 12:47:39 +0200

    Use procfs directly instead of 'free' binary for memory statistics, as free's output is locale-dependent (doesn't work when system language is different from english)

--- web/tools/raspbian_ARM.php
+++ web/tools/raspbian_ARM.php
@@ -2,9 +2,9 @@
 # Commands
 $UPTIME="uptime";
 $CPUUSE="ps aux|awk 'NR > 0 { s +=$3 }; END {print \"cpu %\",s}' | awk '{ print $3 }'";
-$MEMTOT="free -m | grep 'Mem' | awk '{print $2}'";
-$MEMUSE="free -m| grep 'Mem' | awk '{print $3}'";
-$MEMFREE="free -m| grep 'Mem' | awk '{print $7}'";
-$DISKUSE="df -h | grep root | awk '{print $2}'"; 
+$MEMTOT="cat /proc/meminfo | grep MemTotal: | awk '{print int($2/1024)}'";
+$MEMUSE="cat /proc/meminfo | grep -oP '^(MemTotal|MemFree|Buffers|Cached|SReclaimable):\s*\K[0-9]*' | tr '\n' ' ' | awk '{print int(($1-$2-$3-$4-$5)/1024)}'";
+$MEMFREE="cat /proc/meminfo | grep MemAvailable: | awk '{print int($2/1024)}'";
+$DISKUSE="df -h | grep root | awk '{print $2}'";
 $DISKFREE="df -h | grep root | awk '{print $4}'";
 ?>
